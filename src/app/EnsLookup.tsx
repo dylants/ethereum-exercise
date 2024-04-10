c44fcab327df8397a70cde120f013fdda5209ef8
Dylan Smith <dylantsmith@gmail.com>
2024-04-10 16:07:53 -0500

    feat: styling updates for ENS lookup
    - update the look and feel of the ENS lookup component to better match the design, adding in the Button and Input components and some styling

--- src/app/EnsLookup.tsx
+++ src/app/EnsLookup.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { useCallback, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useEnsName } from 'wagmi';
@@ -11,7 +13,12 @@ export type EnsFormInput = {
 export default function EnsLookup() {
   const [address, setAddress] = useState<`0x${string}` | undefined>();
 
-  const { handleSubmit, register, reset } = useForm<EnsFormInput>();
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+    reset,
+  } = useForm<EnsFormInput>();
 
   const onSubmit = useCallback(
     (data: EnsFormInput) => {
@@ -24,22 +31,30 @@ export default function EnsLookup() {
   const { data: ensName } = useEnsName({ address });
 
   return (
-    <>
-      <div>
-        <h2>Find an ENS</h2>
+    <div className="flex gap-8">
+      <div className="flex flex-col border border-slate-400 rounded p-4 w-[300px]">
+        <h2 className="font-bold">Find an ENS</h2>
 
         <form onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor="address">Address:</label>
-          <input
-            type="text"
-            placeholder="0x12345..."
-            {...register('address', { required: true })}
-          />
-          <button type="submit">Submit</button>
+          <div className="flex flex-col gap-2 mt-4">
+            <div>
+              <label className="text-sm" htmlFor="address">
+                Address:
+              </label>
+              <Input
+                type="text"
+                placeholder="0x12345..."
+                variant={errors.address ? 'error' : 'default'}
+                {...register('address', { required: true })}
+              />
+            </div>
+            <Button variant="default" type="submit">
+              Submit
+            </Button>
+          </div>
         </form>
-
-        {ensName && <div>ENS name: {ensName}</div>}
       </div>
-    </>
+      <div>{ensName && <div>ENS name: {ensName}</div>}</div>
+    </div>
   );
 }
