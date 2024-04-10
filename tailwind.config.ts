fe5a5a0d3b6a7669cf51986bd18bc3d83de1d1c3
Dylan Smith <dylantsmith@gmail.com>
2024-04-10 16:05:51 -0500

    feat: shadcn components: button and input
    - initialize shadcn in the application, adding some dependencies and updating the tailwind configuration
    - add some shadcn components: button and input
    - add some core css to the app layout and globals

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,32 @@
 import type { Config } from 'tailwindcss';
 
 const config = {
-  content: [
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  plugins: [],
+  content: ['./src/components/**/*.{ts,tsx}', './src/app/**/*.{ts,tsx}'],
+  plugins: [require('tailwindcss-animate')],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+    },
   },
 } satisfies Config;
 
